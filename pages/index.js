81536d9288708c613647cd931f3c0880e1d3038d
bianca-tm <tech.btovalin@gmail.com>
2024-09-30 21:27:38 -0400

    jucie starter

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>jucie!</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://cdn.tailwindcss.com"></script>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl m-5">jucie</h1>
+        <p className="description ">
+          so you wanna learn about fruits?
         </p>
+        <div class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10 m-5">get jiggy</div>
       </main>
-
-      <Footer />
     </div>
   )
 }
